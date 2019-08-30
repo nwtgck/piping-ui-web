45f08adbcdfc4cbbce3c8ab53d3f0d3772de4396
Ryo Ota <nwtgck@gmail.com>
2019-08-30 14:29:20 +0900

    [add] Support dark theme

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,13 @@
 <template>
-  <v-app>
+  <v-app :dark="setTheme">
     <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+      <v-toolbar-title>
+        <span>Piping </span>
+        <span class="font-weight-light">UI</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+      <!-- FIXME: margin-top is not good solution for adjusting middle in the bar -->
+      <v-switch :label="`Dark Theme`" v-model="enableDarkTheme" style="margin-top: 1.5em;"></v-switch>
     </v-app-bar>
 
     <v-content>
@@ -30,8 +25,19 @@ export default Vue.extend({
   components: {
     HelloWorld,
   },
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      enableDarkTheme: false
+    };
+  },
+  computed: {
+    setTheme() {
+      if (this.enableDarkTheme) {
+        return (this.$vuetify.theme.dark = true);
+      } else {
+        return (this.$vuetify.theme.dark = false);
+      }
+    }
+  }
 });
 </script>
