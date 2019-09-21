0644882b51fa18871cd44eba0eb343bc5b77d585
Ryo Ota <nwtgck@gmail.com>
2019-09-21 19:57:06 +0900

    [change] Remove prefixes on Local Storage keys

--- src/local-storage-keys.ts
+++ src/local-storage-keys.ts
@@ -1,7 +1,7 @@
 export const keys = {
-  darkTheme: 'LOCAL_STORAGE_DARK_THEME',
-  userInputServerUrls: 'LOCAL_STORAGE_USER_INPUT_SERVER_URLS',
-  userInputSecretPaths: 'LOCAL_STORAGE_USER_INPUT_SECRET_PATHS',
-  language: 'LOCAL_STORAGE_LANGUAGE',
-  selectedServerUrl: 'LOCAL_STORAGE_SELECTED_SERVER_URL',
+  darkTheme: 'DARK_THEME',
+  userInputServerUrls: 'USER_INPUT_SERVER_URLS',
+  userInputSecretPaths: 'USER_INPUT_SECRET_PATHS',
+  language: 'LANGUAGE',
+  selectedServerUrl: 'SELECTED_SERVER_URL',
 };
