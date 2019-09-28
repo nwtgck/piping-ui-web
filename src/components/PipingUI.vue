2e2237dd5ea0a3b36a7358211638c6d1b3fd7118
Ryo Ota <nwtgck@gmail.com>
2019-09-28 17:07:28 +0900

    [add] Create UI for password protection

--- src/components/PipingUI.vue
+++ src/components/PipingUI.vue
@@ -90,6 +90,21 @@
           </v-chip>
         </div>
 
+        <v-layout>
+          <v-switch v-model="enablePasswordProtection"
+                    inset
+                    :label="strings['protect_with_password']"
+                    color="blue"
+                    style="padding-left: 0.5em;"/>
+
+          <v-text-field :style="{visibility: enablePasswordProtection ? 'visible' : 'hidden'}"
+                        type="password"
+                        :label="strings['password']"
+                        single-line
+                        outlined
+                        style="margin-left: 0.5em;" />
+        </v-layout>
+
         <v-btn v-if="sendOrGet === 'send'"
                color="primary"
                v-on:click="send()"
@@ -220,6 +235,7 @@ export default class PipingUI extends Vue {
   private files: FilePondFile[] = [];
   private serverUrlHistory: string[] = [];
   private secretPathHistory: string[] = [];
+  private enablePasswordProtection: boolean = false;
 
   // Random strings for suggested secret paths
   private randomStrs: [string] = [
