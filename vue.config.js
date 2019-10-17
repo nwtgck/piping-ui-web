8df85ec095509de4e67280de33727d0ff25e5080
Ryo Ota <nwtgck@gmail.com>
2019-10-17 23:17:08 +0900

    [change] Generate CSS map because auto-generated precache-manifest...js has .css.map files

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  // (base: https://stackoverflow.com/q/56127998/2885946)
+  css: {
+    sourceMap: true
+  },
   // (from: https://medium.com/@dougallrich/give-users-control-over-app-updates-in-vue-cli-3-pwas-20453aedc1f2)
   pwa: {
     workboxPluginMode: 'InjectManifest',
