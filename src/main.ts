7f6327f4a93c6fbe00255f61002d69d1f6b48c6e
Ryo Ota <nwtgck@gmail.com>
2019-11-15 13:19:19 +0900

    [add] Add <meta name="description" content="..."> dynamically

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,23 @@ import AsyncComputed from 'vue-async-computed';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {strings} from "@/strings";
 
+(() => {
+  // TODO: Hard code 'lang'
+  const lang = new URLSearchParams(window.location.search).get('lang');
+  if (lang !== null) {
+    const html = document.getElementsByTagName('html');
+    // Set <html lang>
+    html.item(0)!.lang = lang;
+
+    // Add <meta name="description" content="..."> dynamically
+    const meta = document.createElement('meta');
+    meta.name = 'description';
+    meta.content = strings(lang)['description'];
+    document.head.appendChild(meta);
+  }
+})();
 
 library.add(faGithub);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
