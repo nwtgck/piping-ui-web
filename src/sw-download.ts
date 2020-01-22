eb3d857bcac49d879184e41b647e04692f0a03be
Ryo Ota <nwtgck@nwtgck.org>
2020-01-22 19:48:35 +0900

    [change] Improve logic of supportsSwDownload to use navigator.serviceWorker.ready

--- src/sw-download.ts
+++ src/sw-download.ts
@@ -4,21 +4,25 @@ export const supportsSwDownload: Promise<boolean> = (async () => {
   if (typeof (window as any).safari !== 'undefined') {
     return false;
   }
-  let res: Response | undefined;
+  if (!("serviceWorker" in navigator)) {
+    return false;
+  }
+  // Wait until Service Worker is active
+  await navigator.serviceWorker.ready;
   for (let retryLimit = 3; retryLimit !== 0; retryLimit--) {
+    // Sleep
+    await new Promise(resolve => setTimeout(resolve, 500));
     try {
       // Check whether response can be received
-      res = await fetch('/sw-download-support');
+      const res = await fetch('/sw-download-support');
+      // Success
+      if (res.status === 200) {
+        return await res.text() === 'OK';
+      }
     } catch(err) {
       // Noop
       (() => {})();
     }
-    // Success
-    if (res !== undefined && res.status === 200) {
-      return await res.text() === 'OK';
-    }
-    // Sleep
-    await new Promise(resolve => setTimeout(resolve, 500));
   }
   return false;
 })();
