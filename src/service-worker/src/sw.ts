7de0a76ce794c5ba4d93ababfe38dcffb6bc1e81
Ryo Ota <nwtgck@nwtgck.org>
2020-02-01 19:58:04 +0900

    [change] Remove "@ts-nocheck" and add typing

--- src/service-worker/src/sw.ts
+++ src/service-worker/src/sw.ts
@@ -1,5 +1,12 @@
 // (from: https://medium.com/@dougallrich/give-users-control-over-app-updates-in-vue-cli-3-pwas-20453aedc1f2)
-// @ts-nocheck
+
+// TODO: Not use any
+declare var self: any;
+// TODO: Not use any
+declare var openpgp: any;
+// TODO: Not use any
+declare var workbox: any;
+
 // Backup the native ReadableStream because OpenPGP.js might modify it on Firefox
 const NativeReadableStream = ReadableStream;
 import {createReadableStreamWrapper} from '@mattiasbuelens/web-streams-adapter';
@@ -10,7 +17,7 @@ const toPolyfillReadable = createReadableStreamWrapper(ReadableStream);
 const toNativeReadable = createReadableStreamWrapper(NativeReadableStream);
 
 // Generate random string with specific length
-function generateRandomString(len){
+function generateRandomString(len: number): string {
   const alphas  = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   const chars   = [...alphas, ...numbers];
@@ -19,11 +26,22 @@ function generateRandomString(len){
   return [...randomArr].map(n => chars[n % chars.length]).join('');
 }
 
+// TODO: duplicate code
+type Protection = {type: 'raw'} | {type: 'string', key: string} | {type: 'uint8array', key: string};
+
+// TODO: Use io-ts
+type DownloadInfo = {
+  url: string,
+  filename: string,
+  protection: Protection,
+  decryptErrorMessage: string,
+};
+
 // ID => download-info
-const idToDownloadInfo = {};
+const idToDownloadInfo: {[id: string]: DownloadInfo} = {};
 
 // Generate unique download-info ID
-function generateUniqueDownloadInfoId() {
+function generateUniqueDownloadInfoId(): string {
   // eslint-disable-next-line no-constant-condition
   while(true) {
     const downloadInfoId = generateRandomString(128);
@@ -34,16 +52,17 @@ function generateUniqueDownloadInfoId() {
 }
 
 // (from: https://gist.github.com/borismus/1032746#gistcomment-1493026)
-function base64ToUint8Array(base64Str) {
+function base64ToUint8Array(base64Str: string): Uint8Array {
   const raw = atob(base64Str);
-  return Uint8Array.from(Array.prototype.map.call(raw, (x) => {
+  return Uint8Array.from(Array.prototype.map.call(raw, (x: string) => {
     return x.charCodeAt(0);
-  }));
+  }) as number[]); // TODO: Not use type assertion
 }
 
 // This is the code piece that GenerateSW mode can't provide for us.
 // This code listens for the user's confirmation to update the app.
-self.addEventListener('message', (e) => {
+// TODO: Not use any
+self.addEventListener('message', (e: any) => {
   if (!e.data) {
     return;
   }
@@ -91,13 +110,14 @@ self.addEventListener('message', (e) => {
 });
 
 // Support for stream download
-self.addEventListener('fetch', (event) => {
+// TODO: Not use any
+self.addEventListener('fetch', (event: any) => {
   const url = new URL(event.request.url);
   if (url.pathname === '/sw-download-support') {
     // Return "OK"
     event.respondWith(new Response(
       new NativeReadableStream({
-        start(controller) {
+        start(controller: ReadableStreamDefaultController) {
           controller.enqueue(new Uint8Array([79, 75]));
           controller.close();
         }
@@ -130,14 +150,12 @@ self.addEventListener('fetch', (event) => {
           return protection.key;
         case "uint8array":
           return base64ToUint8Array(protection.key);
-        default:
-          console.error(`Unexpected protection.type: ${protection.type}`);
-          return undefined;
       }
     })();
 
     event.respondWith((async () => {
       const res = await fetch(targetUrl);
+      // @ts-ignore to solve an error: TS2339: Property 'entries' does not exist on type 'Headers'.
       const headers = new Headers([...res.headers.entries()]);
       // (from: https://github.com/jimmywarting/StreamSaver.js/blob/314e64b8984484a3e8d39822c9b86a345eb36454/sw.js#L120-L122)
       // Make filename RFC5987 compatible
@@ -153,7 +171,7 @@ self.addEventListener('fetch', (event) => {
           openpgp.config.allow_unauthenticated_stream = true;
           // Decrypt the response body
           const decrypted = await openpgp.decrypt({
-            message: await openpgp.message.read(toPolyfillReadable(res.body)),
+            message: await openpgp.message.read(toPolyfillReadable(res.body!)),
             passwords: [password],
             format: 'binary'
           });
@@ -167,7 +185,7 @@ self.addEventListener('fetch', (event) => {
         }
       }
 
-      const downloadableRes = new Response(toNativeReadable(plainStream), {
+      const downloadableRes = new Response(toNativeReadable(plainStream!) as ReadableStream, {
         headers
       });
       return downloadableRes;
