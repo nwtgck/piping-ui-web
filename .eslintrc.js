cb716d92cff068b53e5a7ee43dc140074eb09c03
Ryo Ota <nwtgck@nwtgck.org>
2020-02-07 20:28:59 +0900

    lint: fix lint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,8 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-unused-vars': 'off',
   },
   parserOptions: {
     parser: '@typescript-eslint/parser'
