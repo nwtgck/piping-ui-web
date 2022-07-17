a3ea6348ef26cf22fe9ce3f2dc606bfabbecf83c
Ryo Ota <nwtgck@nwtgck.org>
2022-07-17 11:47:28 +0900

    fix PWA setting

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack');
+const {InjectManifest} = require('workbox-webpack-plugin');
 
 module.exports = {
   // (from: https://cli.vuejs.org/config/#publicpath)
@@ -24,18 +25,14 @@ module.exports = {
   // (from: https://github.com/vuetifyjs/vuetify/issues/8279#issuecomment-517900297)
   transpileDependencies: ['vuetify'],
   // (from: https://medium.com/@dougallrich/give-users-control-over-app-updates-in-vue-cli-3-pwas-20453aedc1f2)
-  pwa: {
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      swSrc: './src/service-worker/dist/src/sw.js',
-      swDest: 'service-worker.js',
-      importWorkboxFrom: 'local',
-      exclude: [
-        /\.map$/,
-        '_redirects',
-        // Ignore piping-ui.auth caching because the logic can be changeable in the future
-        /piping-ui-auth.*\.js/
-      ],
-    },
-  }
+  pwa: new InjectManifest({
+    swSrc: './src/service-worker/dist/src/sw.js',
+    swDest: 'service-worker.js',
+    exclude: [
+      /\.map$/,
+      '_redirects',
+      // Ignore piping-ui.auth caching because the logic can be changeable in the future
+      /piping-ui-auth.*\.js/
+    ],
+  }),
 };
