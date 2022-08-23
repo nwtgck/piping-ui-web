ab8b544abbed6c23a35e6ff1c591bcd164e15b87
Ryo Ota <nwtgck@nwtgck.org>
2022-08-24 08:03:02 +0900

    create strings ref

---
+++ src/strings/strings.ts
@@ -0,0 +1,30 @@
+import {ref, watch, type Ref} from "vue";
+
+import {language} from "@/language"
+import {type Strings} from "@/strings/en";
+const enAsync = () => import("./en").then(p => p.en);
+const jaAsync = () => import("./ja").then(p => p.ja);
+const defaultAsync = enAsync;
+
+export const strings: Ref<Strings | undefined> = (() => {
+  const strings = ref<Strings>();
+
+  async function updateLang() {
+    if(language.value.startsWith("en")) {
+      strings.value = await enAsync();
+      return;
+    }
+    if(language.value.startsWith("ja")) {
+      strings.value = await jaAsync();
+      return;
+    }
+    strings.value = await defaultAsync();
+  }
+  updateLang();
+
+  watch(language, async () => {
+    await updateLang();
+  });
+
+  return strings;
+})();
