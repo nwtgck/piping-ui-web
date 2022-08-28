073d52dd723e7188d305df2ade4cd8ddef061ca2
Ryo Ota <nwtgck@nwtgck.org>
2022-08-28 23:24:51 +0900

    refactor: simplify generateRandomString() in Service Worker

--- src/service-worker/src/service-worker.ts
+++ src/service-worker/src/service-worker.ts
@@ -8,12 +8,9 @@ declare const self: ServiceWorkerGlobalScope;
 
 // Generate random string with specific length
 function generateRandomString(len: number): string {
-  const alphas  = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  const chars   = [...alphas, ...numbers];
-
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   const randomArr = crypto.getRandomValues(new Uint32Array(len));
-  return [...randomArr].map(n => chars[n % chars.length]).join('');
+  return [...randomArr].map(n => chars.charAt(n % chars.length)).join('');
 }
 
 type SwDownload = {
