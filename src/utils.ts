754ee431440834de9d5593b7a631d2390da8801c
Ryo Ota <nwtgck@nwtgck.org>
2022-09-13 19:57:36 +0900

    refactor: makePromise()

--- src/utils.ts
+++ src/utils.ts
@@ -143,8 +143,8 @@ export async function sha256(input: string): Promise<string> {
  * Make a promise which can be resolved and rejected outside of the Promise constructor
  */
 export function makePromise<T>(): {promise: Promise<T>, resolve: (value: T | PromiseLike<T>) => void, reject: (reason?: any) => void} {
-  let resolve = (value: T | PromiseLike<T>) => {};
-  let reject = () => {};
+  let resolve!: (value: T | PromiseLike<T>) => void;
+  let reject!: (reason?: any) => void;
   const promise = new Promise<T>((_resolve, _reject) => {
     resolve = _resolve;
     reject  = _reject;
