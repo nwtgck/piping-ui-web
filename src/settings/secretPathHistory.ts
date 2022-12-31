1901e32761452e333cd085ee420e0ae837d56a24
Ryo Ota <nwtgck@nwtgck.org>
2022-12-31 09:01:50 +0900

    fix: secretPathHistory

--- src/settings/secretPathHistory.ts
+++ src/settings/secretPathHistory.ts
@@ -1,18 +1,23 @@
-import {computed} from "vue";
+import {computed, ref} from "vue";
 import {keys} from "@/local-storage-keys";
 import {loadLocalStorageWithValidation} from "@/utils/loadLocalStorageWithValidation";
 import * as t from "io-ts";
 
+const inner = ref<readonly string[]>((() => {
+  // Load secret path history from local storage
+  const savedSecretPathHistory: string[] | undefined = loadLocalStorageWithValidation(t.array(t.string), keys.secretPathHistory);
+  if (savedSecretPathHistory === undefined) {
+    return [];
+  }
+  return savedSecretPathHistory;
+})());
+
 export const secretPathHistory = computed<readonly string[]>({
   get() {
-    // Load secret path history from local storage
-    const savedSecretPathHistory: string[] | undefined = loadLocalStorageWithValidation(t.array(t.string), keys.secretPathHistory);
-    if (savedSecretPathHistory === undefined) {
-      return [];
-    }
-    return savedSecretPathHistory;
+    return inner.value;
   },
   set(history: readonly string[]) {
     window.localStorage.setItem(keys.secretPathHistory, JSON.stringify(history));
+    inner.value = history;
   },
 });
